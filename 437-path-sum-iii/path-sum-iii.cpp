80bd32e88fa9bb3630e2509a420d39bbaccd0a98
akash-goyal-20 <akashgoyal5020@gmail.com>
2026-06-15 14:22:39 +0530

    Time: 3 ms (85.80%) | Memory: 21.7 MB (26.86%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.cpp
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -1,36 +1,28 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
 class Solution {
 public:
     int total = 0;
+    unordered_map<long long, int> mp;
+
     void f(TreeNode* root, int targetSum, long long currentSum) {
-        if (root == NULL) {
+        if (root == NULL)
             return;
-        }
-        currentSum = currentSum + root->val;
-        if (currentSum == targetSum) {
-            total++;
-        }
+
+        currentSum += root->val;
+
+        total += mp[currentSum - targetSum];
+
+        mp[currentSum]++;
+
         f(root->left, targetSum, currentSum);
         f(root->right, targetSum, currentSum);
+
+        mp[currentSum]--;
     }
+
     int pathSum(TreeNode* root, int targetSum) {
-        if (root == NULL) {
-            return 0;
-        }
+        mp[0] = 1;
+
         f(root, targetSum, 0);
-        pathSum(root->left, targetSum);
-        pathSum(root->right, targetSum);
 
         return total;
     }
