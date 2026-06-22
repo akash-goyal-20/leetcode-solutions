f5fbb1e0c40cafaf038b78901cb6153db75f6fb5
akash-goyal-20 <akashgoyal5020@gmail.com>
2026-06-22 14:40:37 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (39.61%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -3,24 +3,16 @@ public:
     int maxNumberOfBalloons(string text) {
         vector<int> mp(26, 0);
 
-        for (int i = 0; i < text.size(); i++) {
-            int w = text[i] - 'a';
-            mp[w]++;
+        for(char c : text) {
+            mp[c - 'a']++;
         }
 
-        string target = "balloon";
-        int miniOcc = INT_MAX;
-
-        for (int i = 0; i < target.size(); i++) {
-            int w = target[i] - 'a';
-            if (target[i] == 'l' || target[i] == 'o') {
-                miniOcc = min(miniOcc, mp[w] / 2);
-            } else {
-
-                miniOcc = min(miniOcc, mp[w]);
-            }
-        }
-
-        return miniOcc;
+        return min({
+            mp['b' - 'a'],
+            mp['a' - 'a'],
+            mp['l' - 'a'] / 2,
+            mp['o' - 'a'] / 2,
+            mp['n' - 'a']
+        });
     }
 };
