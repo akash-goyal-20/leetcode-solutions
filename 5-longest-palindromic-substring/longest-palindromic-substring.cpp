c64a5268bb9b316919256146fe653af165d81962
akash-goyal-20 <akashgoyal5020@gmail.com>
2026-06-24 23:44:48 +0530

    Time: 365 ms (12.41%) | Memory: 296.1 MB (5.02%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -19,8 +19,8 @@ public:
         int n = s.size();
         vector<vector<int>> dp(n, vector<int>(n, -1));
 
-        for (int i = 0; i < s.size(); i++) {
-            for (int j = i; j < s.size(); j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
                 if (isPalindrome(i, j, s, dp)) {
                     if (j - i + 1 > maxi) {
                         ans = s.substr(i, j - i + 1);
