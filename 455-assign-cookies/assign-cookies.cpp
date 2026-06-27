8c83687a06b781c3a4507d0c957d512b0b2cbd33
akash-goyal-20 <akashgoyal5020@gmail.com>
2026-06-27 12:30:20 +0530

    Time: 11 ms (39.77%) | Memory: 44.9 MB (47.69%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int n = g.size();
+        int m = s.size();
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int cnt = 0;
+        int i = 0;
+        int j = 0;
+
+        while(j < m && i < n){
+            if(s[j] >= g[i]){
+                cnt++;
+                i++;
+            }
+            j++;
+        }
+
+        
+        return cnt;
+    }
+};
