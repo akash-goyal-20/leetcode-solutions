286b6a583ee861d7b6d72d228cc4a35ae661292b
akash-goyal-20 <akashgoyal5020@gmail.com>
2026-07-01 14:29:19 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (83.83%) - LeetSync

---
+++ 953-reverse-only-letters/reverse-only-letters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int left = 0, right = s.size() - 1;
+
+        while (left < right) {
+
+            while (left < right && !isalpha(s[left]))
+                left++;
+
+            while (left < right && !isalpha(s[right]))
+                right--;
+
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
+
+        return s;
+    }
+};
