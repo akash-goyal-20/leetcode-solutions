f430db03efa56da21a3ccf454a4aa83a619c3489
akash-goyal-20 <akashgoyal5020@gmail.com>
2026-07-02 17:06:47 +0530

    Time: 8 ms (7.10%) | Memory: 9.7 MB (76.02%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) {
+            return false;
+        }
+        map<char, int> mp1;
+        map<char, int> mp2;
+
+        for (int i = 0; i < s.size(); i++) {
+            mp1[s[i]]++;
+            mp2[t[i]]++;
+        }
+        for (int i = 0; i < t.size(); i++) {
+            if (mp2[t[i]] != mp1[t[i]]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
