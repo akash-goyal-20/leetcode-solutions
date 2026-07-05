0971c045560767212069544740e4a86723a54460
akash-goyal-20 <akashgoyal5020@gmail.com>
2026-07-06 00:59:36 +0530

    Time: 0 ms (100.00%) | Memory: 63.5 MB (48.12%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int low = 0;
-        int high = arr.size() - 1;
+        int n = arr.size();
+        if (arr[0] > arr[1]) {
+            return 0;
+        }
+        if (arr[n - 1] > arr[n - 2]) {
+            return n - 1;
+        }
 
-        while (low < high) {
+        int low = 1;
+        int high = arr.size() - 2;
+
+        while (low <= high) {
             int mid = low + (high - low) / 2;
-
-            if (arr[mid] < arr[mid + 1]) {
-                // Increasing slope
+            if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+                return mid;
+            } else if (arr[mid] > arr[mid - 1]) {
                 low = mid + 1;
-            } else {
-                // Decreasing slope (or peak)
-                high = mid;
+            } else if (arr[mid] > arr[mid + 1]) {
+                high = mid - 1;
             }
         }
 
-        return low;
+        return -1;
     }
 };
