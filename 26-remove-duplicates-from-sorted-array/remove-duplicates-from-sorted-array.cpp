05c469b5d0140c8e40757d3604d7d1bbc3f98404
akash-goyal-20 <akashgoyal5020@gmail.com>
2026-07-05 23:36:44 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (52.87%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+        int j = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[j] != nums[i]) {
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+
+        return j + 1;
+    }
+};
