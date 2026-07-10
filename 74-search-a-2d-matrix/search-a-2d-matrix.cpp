8d799d66266899d4df34321713edb412cc870f09
akash-goyal-20 <akashgoyal5020@gmail.com>
2026-07-10 20:13:12 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (44.91%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,17 +1,43 @@
 class Solution {
 public:
+    bool binary(int row, vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (matrix[row][mid] == target) {
+                return true;
+            } else if (target > matrix[row][mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size();
         int n = matrix[0].size();
 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(matrix[i][j] == target){
-                    return true;
-                }
+        int low = 0;
+        int high = m - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (binary(mid, matrix, target)) {
+                return true;
+            } else if (target > matrix[mid][n - 1]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
-        
+
         return false;
     }
 };
