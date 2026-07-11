aae68a18a7e39e2954688e497f8b1748f6a21ff7
akash-goyal-20 <akashgoyal5020@gmail.com>
2026-07-11 11:43:19 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (26.31%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.cpp
@@ -0,0 +1,58 @@
+class disjointSet {
+    vector<int> parent;
+    vector<int> rank;
+
+public:
+    disjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findUparent(int n) {
+        if (parent[n] == n) {
+            return parent[n];
+        }
+        return parent[n] = findUparent(parent[n]);
+    }
+
+    void unite(int u, int v) {
+        int ulp_u = findUparent(u);
+        int ulp_v = findUparent(v);
+        if (ulp_u == ulp_v) {
+            return;
+        }
+        if (rank[ulp_u] > rank[ulp_v]) {
+            parent[ulp_v] = ulp_u;
+        } else if (rank[ulp_u < rank[ulp_v]]) {
+            parent[ulp_v] = ulp_u;
+        } else {
+            rank[ulp_u]++;
+            parent[ulp_v] = ulp_u;
+        }
+    }
+};
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        vector<vector<int>> adj(n + 1);
+        vector<int> ans;
+        disjointSet ds(n);
+
+        for (int i = 0; i < n; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            if (ds.findUparent(u) == ds.findUparent(v)) {
+                ans = {u, v};
+            }
+            ds.unite(u, v);
+        }
+
+        return ans;
+    }
+};
