535553848fd90266b66c192d6f30e1a6b8adbdd4
akash-goyal-20 <akashgoyal5020@gmail.com>
2026-07-15 01:50:59 +0530

    Time: 151 ms (88.77%) | Memory: 132.5 MB (15.95%) - LeetSync

---
+++ 204-count-primes/count-primes.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        int cnt = 0;
+        vector<int> prime(n,1);
+
+        for(int i = 2; i*i < n; i++){
+            if(prime[i]==1){
+                for(int j = i*i; j < n; j = j+i){
+                    prime[j] = 0;
+                }
+            }
+        }
+
+        for(int i = 2; i < n; i++){
+            if(prime[i]==1){
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+};
