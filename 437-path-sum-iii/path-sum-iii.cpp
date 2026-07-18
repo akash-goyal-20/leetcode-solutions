7d153835ed093468e909efd4087b68c4d4515ca5
akash-goyal-20 <akashgoyal5020@gmail.com>
2026-07-18 23:13:01 +0530

    Time: 3 ms (85.81%) | Memory: 20.8 MB (44.16%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.cpp
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -1,29 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int total = 0;
-    unordered_map<long long, int> mp;
+    map<long long,int> mp;
+    int cnt = 0;
 
-    void f(TreeNode* root, int targetSum, long long currentSum) {
-        if (root == NULL)
+    void dfs(TreeNode* root, long long pathSum,int targetSum){
+        if(root == NULL){
             return;
+        }
+        pathSum = pathSum + root->val;
+        if(mp.find(pathSum - targetSum)!=mp.end()){
+            cnt = cnt + mp[pathSum - targetSum];
+        }
+        mp[pathSum]++;
+        dfs(root->left,pathSum,targetSum);
+        dfs(root->right,pathSum,targetSum);
 
-        currentSum += root->val;
-
-        total += mp[currentSum - targetSum];
-
-        mp[currentSum]++;
-
-        f(root->left, targetSum, currentSum);
-        f(root->right, targetSum, currentSum);
-
-        mp[currentSum]--;
+        mp[pathSum]--;
     }
-
     int pathSum(TreeNode* root, int targetSum) {
-        mp[0] = 1;
-
-        f(root, targetSum, 0);
-
-        return total;
+        mp[0]++;
+        dfs(root,0,targetSum);
+        return cnt;
     }
 };
