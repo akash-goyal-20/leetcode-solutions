760c9ef2d594a5dec5a476f16eb5fe8315528c04
akash-goyal-20 <akashgoyal5020@gmail.com>
2026-07-19 22:06:41 +0530

    Time: 251 ms (5.12%) | Memory: 22.8 MB (6.08%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    long long f(int index, vector<int>& nums, vector<int>& dp){
-        if(index >= nums.size() - 1)
+    int f(int index, vector<int>& nums, vector<int>& dp) {
+        if (index == nums.size() - 1) {
             return 0;
-
-        if(dp[index] != -1)
-            return dp[index];
-
-        long long mini = INT_MAX;
-
-        for(int jump = 1; jump <= nums[index]; jump++){
-            if(index + jump < nums.size()){
-                mini = min(mini, 1 + f(index + jump, nums, dp));
-            }
         }
-
-        return dp[index] = mini;
+        if (index > nums.size() - 1 || nums[index] == 0) {
+            return 1e9;
+        }
+        if (dp[index] != -1) {
+            return dp[index];
+        }
+        int minJump = 1e9;
+        for (int i = 1; i <= nums[index]; i++) {
+            minJump = min(minJump, f(index + i, nums, dp));
+        }
+        return dp[index] = minJump + 1;
     }
-
     int jump(vector<int>& nums) {
         vector<int> dp(nums.size(), -1);
         return f(0, nums, dp);
