f6ba1670aca922f0684881279dbbe2b3b0e3823d
akash-goyal-20 <akashgoyal5020@gmail.com>
2026-07-25 23:35:43 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (80.27%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        vector<int> ans;
+        int n = nums.size();
 
-        for(int i = 0; i < nums.size(); i++){
-            mp[nums[i]]++;
+        // Variable to store XOR of all elements
+        long XOR = 0;
+
+        for (int i = 0; i < n; i++) {
+            XOR = XOR ^ nums[i];
         }
-        for(auto it : mp){
-            if(it.second == 1){
-                ans.push_back(it.first);
+
+        /* Variable to get the rightmost
+        set bit in overall XOR */
+        int rightmost = (XOR & (XOR - 1)) ^ XOR;
+
+        /* Variables to stores XOR of
+        elements in bucket 1 and 2 */
+        int XOR1 = 0, XOR2 = 0;
+
+        // Traverse the array
+        for (int i = 0; i < n; i++) {
+
+            /* Divide the numbers among bucket 1
+             and 2 based on rightmost set bit */
+            if (nums[i] & rightmost) {
+                XOR1 = XOR1 ^ nums[i];
+            } else {
+                XOR2 = XOR2 ^ nums[i];
             }
         }
-        return ans;
+        return {XOR1, XOR2};
     }
 };
