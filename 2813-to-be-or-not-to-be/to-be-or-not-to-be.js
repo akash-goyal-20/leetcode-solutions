912abbbb9060ee74f3f890f18c4646c88d741175
akash-goyal-20 <akashgoyal5020@gmail.com>
2026-07-29 12:16:43 +0530

    Time: 49 ms (27.06%) | Memory: 53 MB (83.04%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,20 @@
+/**
+ * @param {*} val
+ * @return {{toBe: Function, notToBe: Function}}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(other) {
+            if (val === other) {
+                return true;
+            }
+            throw new Error("Not Equal");
+        },
+        notToBe: function(other) {
+            if (val !== other) {
+                return true;
+            }
+            throw new Error("Equal");
+        }
+    };
+};
