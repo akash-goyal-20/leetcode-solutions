e98a11809f40cdb4c42a2cc4c43aabf90f5c2032
akash-goyal-20 <akashgoyal5020@gmail.com>
2026-07-30 15:21:08 +0530

    Time: 307 ms (29.80%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,6 +1,14 @@
-SELECT (
-    SELECT DISTINCT salary
+-- SELECT (
+--     SELECT DISTINCT salary
+--     FROM Employee
+--     ORDER BY salary DESC
+--     LIMIT 1 OFFSET 1
+-- ) AS SecondHighestSalary;
+
+SELECT max(salary) as 'SecondHighestSalary'
+FROM (
+    SELECT salary,
+           DENSE_RANK() OVER(ORDER BY salary DESC) AS rnk
     FROM Employee
-    ORDER BY salary DESC
-    LIMIT 1 OFFSET 1
-) AS SecondHighestSalary;
+) as t
+WHERE rnk = 2;
