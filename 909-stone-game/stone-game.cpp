1d96848ca4c2eb1ad346f9274db6a62a35c77115
akash-goyal-20 <akashgoyal5020@gmail.com>
2026-08-02 13:46:07 +0530

    Time: 15 ms (20.49%) | Memory: 19.7 MB (20.08%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,7 +1,26 @@
 class Solution {
 public:
-    
+    int f(vector<int>& piles, int i, int j, vector<vector<int>>& dp) {
+        if (i > j) {
+            return 0;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        int take_i =
+            piles[i] + min(f(piles, i + 2, j, dp), f(piles, i + 1, j - 1, dp));
+        int take_j =
+            piles[j] + min(f(piles, i, j - 2, dp), f(piles, i + 1, j - 1, dp));
+
+        return dp[i][j] = max(take_i, take_j);
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n = piles.size();
+        int sum = accumulate(piles.begin(), piles.end(), 0);
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        if (f(piles, 0, n - 1, dp) > sum / 2) {
+            return true;
+        }
+        return false;
     }
 };
