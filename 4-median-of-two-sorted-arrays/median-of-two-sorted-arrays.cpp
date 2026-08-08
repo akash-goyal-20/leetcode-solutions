8f2e5732100cbdd30411fbfe62d053dbf24cc2ae
akash-goyal-20 <akashgoyal5020@gmail.com>
2026-08-08 18:18:58 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (80.15%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,36 +1,80 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans;
-        int l1 = nums1.size();
-        int l2 = nums2.size();
-        int i = 0;
-        int j = 0;
+    double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
 
-        while (i < l1 && j < l2) {
-            if (nums1[i] < nums2[j]) {
-                ans.push_back(nums1[i]);
-                i++;
-            } else {
-                ans.push_back(nums2[j]);
-                j++;
+        int n1 = a.size();
+        int n2 = b.size();
+
+        // Always binary search on the smaller array
+        if (n1 > n2) {
+            return findMedianSortedArrays(b, a);
+        }
+
+        int low = 0;
+        int high = n1;
+
+        // Number of elements that should be on the left side
+        int left = (n1 + n2 + 1) / 2;
+
+        int n = n1 + n2;
+
+        while (low <= high) {
+
+            // Partition of array a
+            int mid1 = (low + high) >> 1;
+
+            // Partition of array b
+            int mid2 = left - mid1;
+
+            // Boundary values
+            int l1 = INT_MIN;
+            int l2 = INT_MIN;
+            int r1 = INT_MAX;
+            int r2 = INT_MAX;
+
+            // Right side of a
+            if (mid1 < n1) {
+                r1 = a[mid1];
+            }
+
+            // Right side of b
+            if (mid2 < n2) {
+                r2 = b[mid2];
+            }
+
+            // Left side of a
+            if (mid1 - 1 >= 0) {
+                l1 = a[mid1 - 1];
+            }
+
+            // Left side of b
+            if (mid2 - 1 >= 0) {
+                l2 = b[mid2 - 1];
+            }
+
+            // Correct partition
+            if (l1 <= r2 && l2 <= r1) {
+
+                // Total number of elements is odd
+                if (n % 2 == 1) {
+                    return max(l1, l2);
+                }
+
+                // Total number of elements is even
+                return (max(l1, l2) + min(r1, r2)) / 2.0;
+            }
+
+            // Too many elements taken from a
+            else if (l1 > r2) {
+                high = mid1 - 1;
+            }
+
+            // Too few elements taken from a
+            else {
+                low = mid1 + 1;
             }
         }
-        while (i < l1) {
-            ans.push_back(nums1[i]);
-            i++;
-        }
-        while (j < l2) {
-            ans.push_back(nums2[j]);
-            j++;
-        }
 
-        int n = ans.size();
-        if (n % 2 == 1) {
-            return ans[n / 2];
-        } else {
-            int k = n / 2;
-            return (double)(ans[k] + ans[k - 1]) / 2;
-        }
+        return 0.0;
     }
 };
