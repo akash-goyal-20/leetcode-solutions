3a2dd79ae7eb25680b044fcab17879e4db5bdbca
akash-goyal-20 <akashgoyal5020@gmail.com>
2026-08-08 17:31:56 +0530

    Time: 7 ms (18.36%) | Memory: 9.7 MB (76.48%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -4,17 +4,23 @@ public:
         if (s.size() != t.size()) {
             return false;
         }
-        map<char, int> mp1;
-        map<char, int> mp2;
+        // map<char, int> mp1;
+        // map<char, int> mp2;
 
-        for (int i = 0; i < s.size(); i++) {
-            mp1[s[i]]++;
-            mp2[t[i]]++;
-        }
-        for (int i = 0; i < t.size(); i++) {
-            if (mp2[t[i]] != mp1[t[i]]) {
-                return false;
-            }
+        // for (int i = 0; i < s.size(); i++) {
+        //     mp1[s[i]]++;
+        //     mp2[t[i]]++;
+        // }
+        // for (int i = 0; i < t.size(); i++) {
+        //     if (mp2[t[i]] != mp1[t[i]]) {
+        //         return false;
+        //     }
+        // }
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+
+        if(s != t){
+            return false;
         }
 
         return true;
