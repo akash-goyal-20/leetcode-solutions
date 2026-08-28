6869371b3f1cba3e3eda2b70be1b1a6e61025f0e
akash-goyal-20 <akashgoyal5020@gmail.com>
2026-08-28 17:52:13 +0530

    Time: 22 ms (42.92%) | Memory: 36.1 MB (56.75%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -3,17 +3,13 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
-
-        vector<vector<int>> vis(m, vector<int>(n, 0));
-        vector<vector<int>> dis(m, vector<int>(n, 0));
-
-        queue<pair<int, int>> q;
+        queue<pair<int, pair<int, int>>> q;
+        vector<vector<int>> ans(m, vector<int>(n, 0));
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (mat[i][j] == 0) {
-                    vis[i][j] = 1;
-                    q.push({i, j});
+                    q.push({0, {i, j}});
                 }
             }
         }
@@ -21,22 +17,23 @@ public:
         int col[] = {0, 1, 0, -1};
 
         while (!q.empty()) {
-            int r = q.front().first;
-            int c = q.front().second;
+            int len = q.front().first;
+            int r = q.front().second.first;
+            int c = q.front().second.second;
+
             q.pop();
 
             for (int i = 0; i < 4; i++) {
-                int u = r + row[i];
-                int v = c + col[i];
+                int u = row[i] + r;
+                int v = col[i] + c;
 
-                if (u >= 0 && u < m && v >= 0 && v < n && vis[u][v] == 0) {
-                    dis[u][v] = dis[r][c] + 1;
-                    vis[u][v] = 1;
-                    q.push({u, v});
+                if (u >= 0 && u < m && v >= 0 && v < n && mat[u][v] == 1) {
+                    q.push({len + 1, {u, v}});
+                    mat[u][v] = 0;
+                    ans[u][v] = ans[r][c] + 1;
                 }
             }
         }
-
-        return dis;
+        return ans;
     }
 };
